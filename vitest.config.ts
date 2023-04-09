127468eb25e2456e09c595f33962bc3e6d2e5dfd
Arison Reis <arisonreis157@gmail.com>
2023-04-09 06:32:41 -0300

    adding the config for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     reporters: ['default'],
+    globals: true,
+    onConsoleLog(log, type) {
+      // eslint-disable-next-line no-console
+      console.log(`${log}: ${type}`);
+    },
+    dangerouslyIgnoreUnhandledErrors: true,
   },
 });
