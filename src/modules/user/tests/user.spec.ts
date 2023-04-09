4674f2f4310b01c2f7ef092a399736453ed361a0
Arison Reis <arisontechp@gmail.com>
2023-04-09 05:15:43 -0300

    testing the middlewares and services

--- src/modules/user/tests/user.spec.ts
+++ src/modules/user/tests/user.spec.ts
@@ -1,27 +1,20 @@
 import { test, expect, describe } from 'vitest';
+import { CreateUserService } from '../services/createUserService';
+import { GetUserByEmail } from '../services/getUserByEmail';
 import axios from 'axios';
+import { AppError } from '../../../shared/errors';
 
-describe('Testing the http Router', async () => {
-  test('checking that the controller is working properly for correct data', async () => {
-    const fetching = await axios.post('http://localhost:4000/user/create', {
-      name: 'blabla',
-      email: 'blabla',
-      perfil_url: 'blabla',
-    });
-    expect(fetching.status).toBe(200);
-  });
-
-  test('checking that the controller is working properly for bad data', async () => {
-    const req = fetch('http://localhost:4000/user/create', {
-      method: 'POST',
-      body: JSON.stringify({
-        name: '',
-        email: '',
-        perfil_url: '',
-      }),
-    });
-    const status = (await req).status;
-
-    expect(status).toBe(400);
+describe('http Tests', () => {
+  test('Must not create a new user', async () => {
+    const data = {
+      name: '',
+      email: '',
+      perfil_url: '',
+    };
+    const createUser = async () => {
+      return (await axios.post('http://localhost:4000/user/create', data))
+        .status;
+    };
+    expect(createUser()).resolves.toBe(400);
   });
 });
