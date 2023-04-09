3025cbaae8e64bd89531b0dab51bfb1a85a9508c
Arison Reis <arisonreis157@gmail.com>
2023-04-09 06:36:31 -0300

    create controller to handle user data

--- src/modules/user/controllers/userController.ts
+++ src/modules/user/controllers/userController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from 'express';
 import { IUserData } from '../../../shared/types/UserDatacreate';
 import { AppError } from '../../../shared/errors';
+import { prisma } from '../../../shared/prisma';
+import { CreateUserService } from '../services/createUserService';
 export class UserController {
   public async create(req: Request, res: Response) {
     const { name, email, perfil_url }: IUserData = req.body;
@@ -8,13 +10,28 @@ export class UserController {
     if (!name && !email && !perfil_url) {
       throw new AppError('No data has been reported', 400);
     }
+    const creatingUser = await new CreateUserService()
+      .execute({
+        name: name,
+        email: email,
+        perfil_url: perfil_url,
+      })
+      .catch((err) => {
+        throw new AppError(err.message, err.status);
+      });
 
-    return res.status(200).json({ message: 'ok' });
+    if (creatingUser) {
+      return res.status(200).json({
+        status: 'sucess',
+        message: 'user created successfully',
+      });
+    }
   }
 
-  public async GetAll(){
-    
+  public async GetAll() {
+    const users = await prisma.userCreated.findMany();
+    if (users) {
+      return users;
+    }
   }
-
-
 }
