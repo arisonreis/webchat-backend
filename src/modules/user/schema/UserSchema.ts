fc5baf722eff77b04d4620508e524397861af3f0
Arison Reis <arisonreis157@gmail.com>
2023-04-09 06:38:36 -0300

    creating user data schema with zod

---
+++ src/modules/user/schema/UserSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const dataSchema = z.object({
+  name: z.string().max(100, 'maximum 100 letters').min(1, 'min 1 letter'),
+  email: z
+    .string({ invalid_type_error: 'invalid type' })
+    .email({ message: 'invalid Format' })
+    .optional(),
+  perfil_url: z
+    .string({ invalid_type_error: 'invalid type' })
+    .url({ message: 'invalid Format' })
+    .optional(),
+});
