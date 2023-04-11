2bccca62c434ef6ad6b6653b2287cdec65ede0ec
Arison Reis <arisonreis157@gmail.com>
2023-04-11 06:11:14 -0300

    ✨ feat(userController.ts): add new methods to handle user requests 🐛 fix(userController.ts): change status code to 201 when creating a new user 🐛 fix(userController.ts): change status code to 400 when user is not found 🐛 fix(userController.ts): change status code to 202 when deleting a user ✨ feat(userMiddleware.ts): add new methods to validate request params and body

--- src/modules/user/controllers/userController.ts
+++ src/modules/user/controllers/userController.ts
@@ -1,9 +1,11 @@
 import type { Request, Response } from 'express';
 import { IUserData } from '../../../shared/types/UserDatacreate';
 import { AppError } from '../../../shared/errors';
-import { prisma } from '../../../shared/prisma';
 import { CreateUserService } from '../services/createUserService';
 import { GetAllUsersService } from '../services/getAllUsersService';
+import { GetUserByEmailService } from '../services/getUserByEmailService';
+import { GetUserByIdService } from '../services/getUserByIdService';
+import { DeletUserService } from '../services/deleteUserService';
 export class UserController {
   public async create(req: Request, res: Response) {
     const { name, email, perfil_url }: IUserData = req.body;
@@ -22,7 +24,7 @@ export class UserController {
       });
 
     if (creatingUser) {
-      return res.status(200).json({
+      return res.status(201).json({
         status: 'sucess',
         message: 'user created successfully',
       });
@@ -31,8 +33,62 @@ export class UserController {
 
   public async GetAll(req: Request, res: Response) {
     const getAllUsers = await new GetAllUsersService().execute();
-    if (getAllUsers) {
-      return res.json(getAllUsers);
+    if (getAllUsers.length >= 1) {
+      return res.status(200).json(getAllUsers);
+    } else {
+      return res.status(200).json({
+        status: 'sucess',
+        message: 'there are no users in the database',
+      });
+    }
+  }
+
+  public async GetUserByEmail(req: Request, res: Response) {
+    const { email } = req.params;
+
+    const getUserByEmail = await new GetUserByEmailService().execute({ email });
+
+    if (getUserByEmail) {
+      return res.status(200).json(getUserByEmail);
+    }
+
+    if (!getUserByEmail) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'User not found',
+      });
+    }
+  }
+
+  public async GetById(req: Request, res: Response) {
+    const { id } = req.params;
+    const getUserById = await new GetUserByIdService().execute({ id: id });
+
+    if (getUserById) {
+      return res.status(200).json(getUserById);
+    } else {
+      return res.status(400).json({
+        status: 'error',
+        message: 'user not found',
+      });
+    }
+  }
+
+  public async Delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const userExists = await new GetUserByIdService().execute({ id: id });
+
+    if (userExists) {
+      const deletingUser = await new DeletUserService().execute({ id: id });
+      if (deletingUser) {
+        return res.status(202).send();
+      }else{
+        return res.status(400).json({
+          status:"error",
+          message:"User not found",
+        })
+      }
     }
   }
 }
--- src/modules/user/middlewares/userMiddleware.ts
+++ src/modules/user/middlewares/userMiddleware.ts
@@ -1,9 +1,22 @@
 import type { NextFunction, Request, Response } from 'express';
-import { dataSchema } from '../schema/UserSchema';
+import { paramSchema, dataSchema, emailSchema } from '../schema/UserSchema';
 import { AppError } from '../../../shared/errors';
 export class UserMiddleware {
-  execute(req: Request, res: Response, next: NextFunction) {
+  validateBody(req: Request, res: Response, next: NextFunction) {
+    if (req.method !== 'POST') {
+      throw new AppError('Method not accepted ');
+    }
     dataSchema.parse(req.body);
+    next();
+  }
+
+  validateParams(req: Request, res: Response, next: NextFunction) {
+    paramSchema.parse(req.params);
+    next();
+  }
+
+  validateEmailParam(req: Request, res: Response, next: NextFunction) {
+    emailSchema.parse(req.params);
     next();
   }
 }
