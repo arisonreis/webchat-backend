908918b26568a9101509227a0fea9ed35c64fc13
Arison Reis <arisonreis157@gmail.com>
2023-04-25 23:37:14 -0300

    🔇 chore(http.ts): remove console.log statement in error middleware

--- src/shared/http/http.ts
+++ src/shared/http/http.ts
@@ -13,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log(error)
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: 'error',
